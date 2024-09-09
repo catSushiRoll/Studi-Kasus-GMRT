c5b103aa92e0edc7e0f5a55bf07dcc3d92930bd8
catSushiRoll <163240230+catSushiRoll@users.noreply.github.com>
2024-09-10 00:14:48 +0700

    Create Array.cpp

---
+++ Day 1/Array.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int array [5]={1,2,3,4,5};
+    //int i;
+
+    for (int i=0 ; i<5 ; i++){
+        if (array[i]==3){
+            cout<<array[i];
+        }
+    }
+    return 0;
+}
